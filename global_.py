6b83df8f57d43d10e19fc19846646e4c139ee91e
CrazyComrad <crazycomrad@gmail.com>
2024-01-15 17:55:23 +0400

    dorabotal2

--- global_.py
+++ global_.py
@@ -3,4 +3,4 @@ class paths:
     path_to_save_model = str(pathlib.Path(__file__).resolve().parent) + "\MLflow\Saved_model"
     path_to_model = str(pathlib.Path(__file__).resolve().parent) + "\MLflow\model.pkl"
 
-print()
+
