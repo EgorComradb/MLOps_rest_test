14df6774053b19547447e4a3bbcf3fbe466721f9
EgorComradb <135459138+EgorComradb@users.noreply.github.com>
2024-01-15 17:56:27 +0400

    Update global_.py

--- global_.py
+++ global_.py
@@ -2,5 +2,3 @@ import pathlib
 class paths:
     path_to_save_model = str(pathlib.Path(__file__).resolve().parent) + "\MLflow\Saved_model"
     path_to_model = str(pathlib.Path(__file__).resolve().parent) + "\MLflow\model.pkl"
-
-print()
